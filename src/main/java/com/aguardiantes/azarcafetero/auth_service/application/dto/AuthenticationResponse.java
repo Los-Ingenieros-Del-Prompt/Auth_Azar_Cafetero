f793f1ce2b816392b3b63bc994c0dbd6f2c72ae0
JulianCReal <julian.castiblanco-r@mail.escuelaing.edu.co>
2026-04-10 22:45:20 -0500

    Add userId field to AuthenticationResponse record

--- src/main/java/com/aguardiantes/azarcafetero/auth_service/application/dto/AuthenticationResponse.java
+++ src/main/java/com/aguardiantes/azarcafetero/auth_service/application/dto/AuthenticationResponse.java
@@ -3,6 +3,7 @@ package com.aguardiantes.azarcafetero.auth_service.application.dto;
 
 public record AuthenticationResponse(
         String token,
+        String userId,
         String name,
         String avatarUrl,
         boolean isNewUser
