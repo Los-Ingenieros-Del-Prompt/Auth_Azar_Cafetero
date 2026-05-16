36d7bf0869a2bfaf14ee31578a4b68245df08713
Karol2905 <karol.estupinan-v@mail.escuelaing.edu.co>
2026-05-16 02:00:55 -0500

    fix cors (#21)

--- src/main/java/com/aguardiantes/azarcafetero/auth_service/infrastructure/config/CorsConfig.java
+++ src/main/java/com/aguardiantes/azarcafetero/auth_service/infrastructure/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
-                "https://azarcafetero.vercel.app"
+                "https://azarcafetero.vercel.app","https://azar-cafetero.duckdns.org"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
